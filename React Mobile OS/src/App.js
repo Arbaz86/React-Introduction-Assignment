8bdf093cac6ccb98e331742b15a41d7224365275
Arbaz86 <arbaz10042002@gmail.com>
2022-04-11 15:15:02 +0530

    completed the assignment

--- React Mobile OS/src/App.js
+++ React Mobile OS/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const data = [
+    {
+      heading: "Mobile Operating System",
+      name: [
+        {
+          name: "Android",
+        },
+        {
+          name: "HTC",
+        },
+        {
+          name: "IPhone",
+        },
+        {
+          name: "Windows Phone",
+        },
+      ],
+    },
+    {
+      heading: "Mobile Manufacturers",
+      name: [
+        {
+          name: "Samsung",
+          style: "square",
+        },
+        {
+          name: "BlackBerry",
+          style: "square",
+        },
+        {
+          name: "Micromax",
+        },
+        {
+          name: "Apple",
+          style: "cirle",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((el) => (
+        <>
+          <h1 className="heading">{el.heading}</h1>
+          {el.name.map((el) => (
+            <li className={el.style}>{el.name}</li>
+          ))}
+        </>
+      ))}
     </div>
   );
 }
